c0d67063d8dd1872b68bfffdf41207553fbcde87
EranToutian <49152654+EranToutian@users.noreply.github.com>
2020-03-05 16:49:14 +0200

    Add files via upload

---
+++ src/IDs.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IDs 
+{
+	public static List<String> getIDs()
+	{
+		List<String> ids = new ArrayList<String>();
+		
+		String id1 = "111111111";
+		String id2 = "222222222";
+		//String id3 = "123456789";
+		
+		ids.add(id1);
+		ids.add(id2);
+		//ids.add(id3);
+		
+		return ids;
+	}
+}
